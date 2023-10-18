2acc27cddd82374862fc0d327529a922bd35a90d
Kick-Buttovski <otebulatov@mail.ru>
2023-10-18 23:06:45 +0300

    Initial commit

---
+++ src/main/java/web/service/UserServiceImp.java
@@ -0,0 +1,41 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.dao.UserDao;
+import web.model.User;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+    @Override
+    public User getUser(long id) {
+        return userDao.getUser(id);
+    }
+
+    @Override
+    public List<User> listUsers() {
+        return userDao.listUsers();
+    }
+
+    @Transactional
+    @Override
+    public void addUser(User user) {
+        userDao.addUser(user);
+    }
+    @Transactional
+    @Override
+    public void updateUser(User user) {
+        userDao.updateUser(user);
+    }
+    @Transactional
+    @Override
+    public void deleteUser(long id) {
+        userDao.deleteUser(id);
+    }
+}
